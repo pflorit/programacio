abd1638a500ddb774ad9e8ee04d978297936d65a
pflorit <45623457+pflorit@users.noreply.github.com>
2019-03-22 16:39:09 +0100

    Add files via upload

---
+++ zFlappyBird/src/flappyBird/Renderer.java
@@ -0,0 +1,24 @@
+package flappyBird;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		try {
+			FlappyBird.flappyBird.repaint(g);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
